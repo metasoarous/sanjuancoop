869e8494f6414b629b949908dd86596caef54996
Christopher Small <metasoarous@gmail.com>
2009-11-17 00:01:18 -0800

    Fixed a couple things in the board_members views (ordering essetialluy)

---
+++ app/helpers/board_member_helper.rb
@@ -0,0 +1,12 @@
+module BoardMemberHelper
+  def last_name(name)
+    name.split("").last
+  end
+  def sort_board_members(collection, by = :last_name)
+    case by
+    when :last_name
+      collection.sort! {|a,b| last_name(a.name) <=> last_name(b.name)}
+    end
+    return  collection
+  end
+end
