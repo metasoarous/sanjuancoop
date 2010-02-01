3ad2a3113be8d2e7c2e9e9b4bafe13abaa2e352f
Christopher Small <cts@Christopher-Smalls-MacBook-Pro.local>
2010-01-31 22:55:55 -0800

    Woops! Forgot the gem.config

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "RedCloth"
   config.gem "mocha"
   config.gem "aasm"
+  config.gem "hoptoad_notifier"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
